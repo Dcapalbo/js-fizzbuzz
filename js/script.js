91e284228121c01c960338467082756ef90f9693
Domenico Capalbo <capalbodomenico@gmail.com>
2020-08-26 14:37:25 +0200

    js-fizzbuzz
    esercizio-completo

--- js/script.js
+++ js/script.js
@@ -6,17 +6,17 @@ for (var i = 0; i < 100; i++) {
   var numero = i + 1;
 
   //pongo le condizioni da rispettare
-  
+
   if (numero % 3 == 0 && numero % 5 == 0) {
-    console.log("FizzBuzz"); // Per i numeri multipli di 3 che di 5 stampi "FizzBuzz"
-  }
-  else if (numero % 3 == 0) {
-    console.log("Fizz"); // per i numeri multipli di 3 stampi “Fizz”
+    console.log("FizzBuzz"); // Per i numeri multipli di 3 che di 5 il programma stampa "FizzBuzz"
   }
   else if (numero % 5 == 0) {
-    console.log("Buzz"); // per i numeri multipli di 5 stampi "Buzz"
+    console.log("Buzz"); // per i numeri multipli di 5 il programma stampa "Buzz"
+  }
+  else if (numero % 3 == 0) {
+    console.log("Fizz"); // per i numeri multipli di 3 il programma stampa “Fizz”
   }
   else {
-    console.log(numero); // per gli altri numeri stampi la variabile numero
+    console.log(numero); // per gli altri numeri il programma stampa la variabile numero
   }
 }
